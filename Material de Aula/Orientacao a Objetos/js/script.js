a8bc5818ee8affe397e4f0e80d73917f7309ae4c
jmateusps16 <jmateusps16@gmail.com>
2022-04-26 19:34:41 -0300

    Orientação a Objetos em JS

---
+++ Material de Aula/Orientacao a Objetos/js/script.js
@@ -0,0 +1,15 @@
+//sem nome
+/* let retangolo = class {
+    constructor (altura, largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+}; */
+
+//nomeada
+/* let retangolo = class retangulo {
+    constructor (altura, largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+}; */
