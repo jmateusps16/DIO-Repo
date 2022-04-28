2ea38ad6b9a9e678dc90739c0707c5035585a5e3
jmateusps16 <jmateusps16@gmail.com>
2022-04-28 18:29:15 -0300

    Estudo de Condicionais - Tomada de Decisoes

--- Material de Aula/Orientacao a Objetos/js/script.js
+++ Material de Aula/Orientacao a Objetos/js/script.js
@@ -13,3 +13,66 @@
         this.largura = largura;
     }
 }; */
+
+//Métodos de Protótipos
+/* class retangulo {
+    constructor(altura, largura) {
+        this.altura = altura; this.largura = largura;
+    }
+    //getter
+    get area() {
+        return this.calculaArea()
+    }
+
+    calculaArea() {
+        return this.altura * this.largura;
+    }
+}
+
+const quadrado = new retangulo(10,10);
+
+console.log(quadrado); */
+
+//Métodos estáticos
+/* class ponto {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distancia(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+
+        return Math.hypot(dx, dy);
+    }
+}
+
+const p1 = new ponto(5, 5);
+const p2 = new ponto(10, 10);
+
+p1.distancia;
+p2.distancia;
+
+console.log(ponto.distancia(p1, p2)); */
+
+//Empacotando com protótipos e métodos estáticos
+/* class = Animal {
+    falar() {
+        return this;
+    }
+    static comer() {
+        return this;
+    }
+}
+
+let obj = new Animal();
+obj.falar();
+let falar = obj.falar;
+falar();
+
+Animal.comer();
+let comer Animal.comer;
+comer(); */
+
+NomeUpe
